07e2c00610cf3a3b3b94d90f5debed2c7a486907
Bryan Cornelius Lauwrence <bryan.cornelius.lauw@gmail.com>
2024-03-17 18:46:03 +0700

    animasi dan penambahan main function

--- src/main.py
+++ src/main.py
@@ -1,44 +1,48 @@
 from util import *
 
 print("Selamat datang di ...") # nanti kasih ascii art atau semacamnya
-x = "y"
-while (x == "y"):
-    print("Silakan memilih algoritma\n1. Divide and Conquer\n2. Brute Force (hanya untuk 3 titik kontrol)\n")
+
+print("Silakan memilih algoritma\n1. Divide and Conquer\n2. Brute Force\n")
+pilihan = input("Pilihan: ")
+while (pilihan != "1" and pilihan != "2"):
+    print()
+    print("Pilihan tidak valid\nMohon pilih '1' atau '2'\n")
+    print("Silakan memilih algoritma\n1. Divide and Conquer\n2. Brute Force\n")
     pilihan = input("Pilihan: ")
-    while (pilihan != "1" or pilihan != "2"):
-        print()
-        print("Pilihan tidak valid\nMohon pilih '1' atau '2'\n")
-        print("Silakan memilih algoritma\n1. Divide and Conquer\n2. Brute Force (hanya untuk 3 titik kontrol)\n")
-        pilihan = input("Pilihan: ")
 
-    if (pilihan == "1"):
-        print("Perhitungan dengan algoritma Divide and Conquer")
-        n = input("Masukkan jumlah titik: ")
-        while (not n.isnumeric()):
-            print("Mohon masukkan angka")
-            n = input("Masukkan jumlah titik: ")
+if (pilihan == "1"):
+    print("\nPerhitungan dengan algoritma Divide and Conquer")
+else: 
+    print("\nPerhitungan dengan algoritma Brute Force")
+
+flags = True
+while (flags):
+    n = input("Masukkan jumlah titik: ")
+    if (not n.isnumeric()):
+        print("Mohon masukkan angka (integer)")
+    elif(int(n) < 3):
+        print("Mohon masukkan angka lebih dari sama dengan 3")
+    else:
+        flags = False
         n = int(n)
-    else: 
-        print("Perhitungan dengan algoritma Brute Force")
-        n = 3
 
-    print("Masukkan titik-titik kontrol")
-    list_control = input_points(n)
+print("\nMasukkan titik-titik kontrol")
+list_control = input_points(n)
 
+i = input("\nMasukkan jumlah iterasi: ")
+while (not i.isnumeric()):
+    print("Mohon masukkan angka (integer)")
     i = input("Masukkan jumlah iterasi: ")
-    while (not i.isnumeric()):
-        print("Mohon masukkan berupa integer")
-        i = input("Masukkan jumlah iterasi: ")
 
-    print("\nMelakukan proses perhitungan...")
-    if (pilihan == "1"):
-        list_result = bezier_curve_n(list_control, int(i))
-    # else: # untuk brute force
-    print("\nPerhitungan selesai\n")
+print("\nMelakukan proses perhitungan...")
+list_x_controls = [point[0] for point in list_control]
+list_y_controls = [point[1] for point in list_control]
+plt.plot(list_x_controls, list_y_controls, 'ro--', label='Control Points', markersize=2.5)
+if (pilihan == "1"): 
+    list_result = dnc_bezier(list_control, int(i))
+else: # untuk brute force
+    list_result = brute_force_bezier(list_control, int(i))
+print("\nPerhitungan selesai\nMenampilkan grafik\n")
 
-    show_graph(list_result, list_control)
-        
-    
-
-
-    
+show_graph(list_result, list_control)
+print("Terima kasih")
--- src/util.py
+++ src/util.py
@@ -7,15 +7,35 @@ from math import comb
 def input_points(num_of_points):
     li = []
     for i in range(num_of_points):
+        print("Titik ke-" + str(i+1))
         x = input("Masukkan sumbu-X ke-"+ str(i+1) + ": ")
         y = input("Masukkan sumbu-Y ke-"+ str(i+1) + ": ")
         while (not (x.isnumeric and y.isnumeric())):
             print("Mohon berikan koordinat dalam bentuk angka")
+            print("Titik ke-" + str(i+1))
             x = input("Masukkan sumbu-X ke-"+ str(i+1) + ": ")
             y = input("Masukkan sumbu-Y ke-"+ str(i+1) + ": ")
-        li.append((int(x),int(y)))
+        li.append((float(x),float(y)))
     return li
 
+def show_graph(list_of_points, list_of_controls):
+    plt.clf()
+    list_x_points = [point[0] for point in list_of_points]
+    list_y_points = [point[1] for point in list_of_points]
+    list_x_controls = [point[0] for point in list_of_controls]
+    list_y_controls = [point[1] for point in list_of_controls]
+    plt.plot(list_x_controls, list_y_controls, 'ro--', label='Control Points', markersize=2.5)
+    plt.plot(list_x_points, list_y_points, 'bo-', label='Bezier Curve', markersize=2.5)
+    plt.legend()
+    plt.title("Bezier Curve")
+    plt.xlabel('X')
+    plt.ylabel('Y')
+    plt.grid(True)
+    plt.show()
+
+'''
+ALGORITMA DIVIDE AND CONQUER
+'''
 # Mencari titik tengah antara dua titik
 def find_middle(p1, p2):
     return ((p1[0] + p2[0]) / 2, (p1[1] + p2[1]) / 2)
@@ -28,7 +48,7 @@ def find_all_middle(list_of_poinst):
     return li
 
 # Mencari titik kontrol kurva bezier
-def bezier_curve_n_helper(list_of_poinst, iteration):
+def dnc_bezier_helper(list_of_poinst, iteration):
     if iteration == 0:
         return []
     else:
@@ -42,55 +62,18 @@ def bezier_curve_n_helper(list_of_poinst, iteration):
             temp.insert(i+1, list_of_poinst[len(list_of_poinst)-1])
             i += 1
         iteration -= 1
+        plt.plot(list_of_poinst[0][0], list_of_poinst[0][1], marker='.', color="black", markersize=2.5)
+        plt.pause(0.01)
         # Merge
-        return (bezier_curve_n_helper(temp[:initial_lenght], iteration) + list_of_poinst + bezier_curve_n_helper(temp[initial_lenght:], iteration))
+        return (dnc_bezier_helper(temp[:initial_lenght], iteration) + list_of_poinst + dnc_bezier_helper(temp[initial_lenght:], iteration))
         
 # Mengembalikan titik awal pada kurva bezier
-def bezier_curve_n(list_of_poinst, iteration):
-    return [list_of_poinst[0]] + bezier_curve_n_helper(list_of_poinst, iteration) + [list_of_poinst[len(list_of_poinst)-1]]
-# Divide and Conquer
-# def bezier_curve_n(points, iteration):
-#     # COMBINE
-#     if iteration == 0:
-#         return points
-#     else:
-#         temp_points = []
-#         #DIVIDE
-#         for i in range(len(points) - 1):
-#             midpoint_x = (points[i][0] + points[i+1][0]) / 2
-#             midpoint_y = (points[i][1] + points[i+1][1]) / 2
-#             temp_points.append((midpoint_x, midpoint_y))
-#         # CONQUER
-#         return bezier_curve_n([points[0]] + temp_points + [points[len(points) - 1]], iteration - 1)
+def dnc_bezier(list_of_poinst, iteration):
+    return [list_of_poinst[0]] + dnc_bezier_helper(list_of_poinst, iteration) + [list_of_poinst[len(list_of_poinst)-1]]
 
-def bezier_curve_helper(Point1, Point2, Point3, iteration):
-    if iteration > 0:
-        # Divide and Conquer
-        mid12 = find_middle(Point1, Point2)
-        mid23 = find_middle(Point2, Point3)
-        mid = find_middle(mid12, mid23)
-        iteration -= 1
-        # Merge
-        return (bezier_curve_helper(Point1, mid12, mid, iteration) + [mid] + bezier_curve_helper(mid, mid23, Point3, iteration))
-    else: return []
-
-def bezier_curve_three(p1, p2, p3, n):
-    return [p1] + (bezier_curve_helper(p1, p2, p3, n)) + [p3]
-
-def show_graph(list_of_points, list_of_controls):
-    list_x_points = [point[0] for point in list_of_points]
-    list_y_points = [point[1] for point in list_of_points]
-    list_x_controls = [point[0] for point in list_of_controls]
-    list_y_controls = [point[1] for point in list_of_controls]
-    plt.plot(list_x_controls, list_y_controls, 'ro-', label='Control Points')
-    plt.plot(list_x_points, list_y_points, 'bo--', label='Bezier Curve')
-    plt.legend()
-    plt.title("Bezier Curve")
-    plt.xlabel('X')
-    plt.ylabel('Y')
-    plt.grid(True)
-    plt.show()
-
+'''
+ALGORITMA BRUTE FORCE
+'''
 def brute_force_bezier(points, iteration):
     pol_degree = len(points) - 1
     num_points = 2**iteration + 1
@@ -107,13 +90,7 @@ def brute_force_bezier(points, iteration):
             x += point[0] * weight
             y += point[1] * weight
         bezier_points.append((x, y))
+        plt.plot(x,y, marker='.', color="black", markersize=2.5)
+        plt.pause(0.01)
 
     return bezier_points
-
-print(bezier_curve_n([(1,3), (3,7), (5,2), (7,8), (4,5)], 2))
-print(brute_force_bezier([(1,3), (3,7), (5,2), (7,8), (4,5)], 2))
-# show_graph([(1,3), (3,7), (5,2), (7,8), (4,5)], (bezier_curve_n([(1,3), (3,7), (5,2), (7,8), (4,5)], 2)))
-# print(bezier_curve_three((1,3), (3,7), (5,2), 2))
-
-
-
